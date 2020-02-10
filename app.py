260e4430a5a12c9ecc26692a77f7de5faad0bf3a
kukasaut0 <47347629+kukasaut0@users.noreply.github.com>
2020-02-10 19:06:50 +0100

    no message

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+<<<<<<< HEAD
     return "Hello World!"
+=======
+    return "Hello World?"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+>>>>>>> parent of 13fe88f... going dash
 
 if __name__ == '__main__':
     app.run()
